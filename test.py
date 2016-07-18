546cc25724408ae2d6284d5b154e8f4dd936f7a7
semihyumusak <semihyumusak@yahoo.com>
2016-07-18 19:43:41 +0100

    test

---
+++ test.py
@@ -0,0 +1,52 @@
+import os.path
+import pickle
+import re
+
+from SPARQLWrapper import SPARQLWrapper, JSON
+from feature_generator_functions import populateFeatureAll
+from feature_generator_functions import k_fold_generator
+from feature_generator_functions import getAttributeWithoutCaching
+
+
+# Load of attribute and test values
+with open('trainingDataset.tsv','r') as f:
+    trainingsetAttributes=[x.strip().split('\t') for x in f][1:]
+with open('testDataset.tsv','r') as f:
+    testsetAttributes=[x.strip().split('\t') for x in f][1:]
+
+# # Query caching prevents the algorithm to send DBpedia requests if it is already in the local storage
+# queryCache = set()
+# with open('querycache.txt','r') as f:
+#     lines = f.readlines()
+#     for line in lines:
+#         queryCache.add(line.replace("\n",""))
+# cacheFile = open('querycache.txt', 'a',encoding='utf-8')
+
+i = 0
+featureListTest = []
+featureListTrain = []
+
+# Initialize Feature Sets
+for row in trainingsetAttributes:
+    URI = row[1].replace('"','')
+    featureDictTrain = {"uri":URI}
+    ID = row[0].replace('"','')
+    featureDictTrain.update({"ID":ID})
+    populateFeatureAll(featureDictTrain)
+    featureListTrain.append(featureDictTrain)
+
+
+with open('trainingDataset.tsv','r') as f:
+    trainingsetLabels=[x.strip().split('\t')[6] for x in f][1:]
+
+X=featureListTrain
+y= trainingsetLabels
+
+from sklearn.svm import SVC
+from sklearn.feature_extraction import DictVectorizer
+vec = DictVectorizer()
+
+
+fit = vec.fit(X)
+
+print (fit.fit.get_feature_names())
