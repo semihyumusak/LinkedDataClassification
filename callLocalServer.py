f3007df7c3e561fec6670dafd2c7ea4e79e0fda5
semihyumusak <semihyumusak@yahoo.com>
2016-07-24 19:11:18 +0100

    -

--- callLocalServer.py
+++ callLocalServer.py
@@ -5,24 +5,24 @@ from feature_generator import Main
 endpoint = "http://localhost:8891/sparql"
 queries = []
 queries.append("SELECT ?p ?o WHERE { <URI> ?p ?o}")
-Main("endpoint",queries)
+Main(endpoint,queries)
 
 queries = []
 queries.append("SELECT DISTINCT ?p ?o WHERE { <URI> ?p1 ?o1. ?o1 ?p ?o}")
-Main("endpoint",queries)
+Main(endpoint,queries)
 
 
 queries = []
 queries.append("SELECT DISTINCT ?p ?o WHERE { <URI> ?p1 ?o1. ?o1 ?p2 ?o2. ?o2 ?p ?o}")
-Main("endpoint",queries)
+Main(endpoint,queries)
 
 queries = []
 queries.append("SELECT DISTINCT ?p ?o WHERE { <URI> ?p ?o}")
 queries.append("SELECT DISTINCT ?p ?o WHERE { <URI> ?p1 ?o1. ?o1 ?p ?o}")
-Main("endpoint",queries)
+Main(endpoint,queries)
 
 queries = []
 queries.append("SELECT DISTINCT ?p ?o WHERE { <URI> ?p ?o}")
 queries.append("SELECT DISTINCT ?p ?o WHERE { <URI> ?p1 ?o1. ?o1 ?p ?o}")
 queries.append("SELECT DISTINCT ?p ?o WHERE { <URI> ?p1 ?o1. ?o1 ?p2 ?o2. ?o2 ?p ?o}")
-Main("endpoint",queries)
+Main(endpoint,queries)
